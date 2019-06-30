7a326dd160704aa2a15287f17824752bd4b1f371
Ben Staton <bstaton@critfc.org>
2019-06-29 21:41:27 -0700

    add more useful functions to utils
    Ultimately, I think I found the solution to the diag_plots() issues - which is what incentivized these improvements in general. These utilities are essential in fixing the (hopefully old) problems!

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,10 @@
-#' Insert a brackets as regex version
+#' Insert escapes on regex brackets
 #'
 #' @param string a character vector
+#' @details searches the contents of a string for the occurrence of a
+#'  square bracket or two, and inserts the necessary escapes for searching
+#'  via regular expression
+#' @note this is NOT a function users will generally use.
 #' @export
 
 ins_regex_bracket = function(string) {
@@ -9,13 +13,64 @@ ins_regex_bracket = function(string) {
   return(out)
 }
 
-#' Remove regex brackets
+#' Remove escapes on regex brackets
 #'
 #' @param string a character vector
+#' @details searches the contents of a string for the occurrence of a
+#'  square bracket or two (that has been escaped), and removes the escaping
+#'  that was necessary for regular expression matching
+#' @note this is NOT a function users will generally use.
 #' @export
 
 rm_regex_bracket = function(string) {
   out = stringr::str_replace(string, "\\\\\\[", "\\[")
   out = stringr::str_replace(out, "\\\\\\]", "\\]")
   return(out)
+}
+
+#' Insert the symbols to lock in a string for matching
+#'
+#' To ensure that a regular expression will match exactly,
+#' it's necessary to specify so - which is what this function does.
+#'
+#' @param string a character vector on to which a carrot and dollar sign
+#'   will be pasted to if not already present
+#' @note this is NOT a function users will generally use.
+#' @export
+
+ins_regex_lock = function(string) {
+  if (!stringr::str_detect(string, "\\^")) out = paste("^", string, sep = "")
+  if (!stringr::str_detect(out, "\\$")) out = paste(out, "$", sep = "")
+  return(out)
+}
+
+#' Remove the symbols that lock in a string for matching
+#'
+#' To ensure that a regular expression will match exactly,
+#' it's necessary to specify so - this function undoes the work of
+#' \code{\link{ins_regex_lock}} - currently just for prettier printing of node names
+#'
+#' @param string a character vector on to which a carrot and dollar sign
+#'   will be removed from if present
+#' @note this is NOT a function users will generally use.
+#' @export
+
+rm_regex_lock = function(string) {
+  out = stringr::str_replace(string, "\\^", "")
+  out = stringr::str_replace(out, "\\$", "")
+  return(out)
+}
+
+#' Extract the base of a node name
+#'
+#' Removes square brackets, numbers, and commas that represent
+#' the index of the node element in question. Returns just the
+#' text portion. E.g, \code{"R\[1\]"} becomes \code{"R"}.
+#'
+#' @param p character vector containing node names
+#' @note this is NOT a function users will generally use.
+#' @export
+
+base_p = function(p) {
+  stringr::str_replace(p, "\\[.+\\]", "")
 }
