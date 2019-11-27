9756bf73fdf459dd7f265124ecc1e83e117f5152
Ben Staton <bstaton@critfc.org>
2019-11-27 11:43:35 -0800

    move id_mat to utils
    also, don't export it

--- R/utils.R
+++ R/utils.R
@@ -86,3 +86,26 @@ mytitle = function(text) {
   text(x = usr[1] - xdiff * 0.02, y = usr[4] + ydiff * 0.11,
        labels = text, font = 2, cex = 1.4, xpd = NA)
 }
+
+#' Quickly extract the chain and iteration IDs
+#'
+#' It is sometimes useful to be able to add these characteristics to a
+#' matrix of derived quantities, which then allows coercion to a \code{mcmc.list}
+#' object, which can then be binded (e.g., \code{\link{post_bind}}) to the
+#' more complete samples.
+#'
+#' @param post an object of class \code{mcmc.list}
+#'
+#' @return a matrix with columns \code{"CHAIN"} and \code{"ITER"}
+#' @note this is NOT a function users will generally use.
+
+id_mat = function(post) {
+  # stop if post isn't mcmc.list
+  if (!coda::is.mcmc.list(post)) {
+    stop ("post must be an object of class 'mcmc.list'")
+  }
+
+  # extract and return the sample identification information
+  as.matrix(post, iters = T, chains = T)[,c("CHAIN", "ITER")]
+}
+
