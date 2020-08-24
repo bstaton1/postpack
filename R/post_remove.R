fb666b06dddb61a6192132b54be4953bdcef5acf
Ben Staton <statonbe@gmail.com>
2020-08-23 19:23:35 -0700

    update p arg to params in post_remove

--- R/post_remove.R
+++ R/post_remove.R
@@ -3,7 +3,7 @@
 #' Just like post_subset, but keep all nodes from \code{post} except nodes that match \code{p}
 #'
 #' @param post an object of class \code{mcmc.list}
-#' @param p a character vector of with length >= 1 specifying the nodes to summarize.
+#' @param params a character vector of with length >= 1 specifying the nodes to summarize.
 #'   Passed to \code{\link{match_params}}, so can (and sometimes should) be a regular expression.
 #' @param ask logical. Do you wish to be prompted prior to removing nodes?
 #'   Defaults to \code{TRUE}
@@ -24,17 +24,17 @@
 #' }
 #' @export
 
-post_remove = function(post, p, ask = TRUE, auto_escape = TRUE) {
+post_remove = function(post, params, ask = TRUE, auto_escape = TRUE) {
 
   # extract the names to discard
-  discard_p = match_params(post, p, ubase = F, auto_escape = auto_escape)
+  discard_params = match_params(post, params, ubase = F, auto_escape = auto_escape)
 
   # print message and ask user if they wish to proceed
   acceptable_yes = c("y", "yes")
   acceptable_no = c("n", "no")
   if (ask) {
     message("Node(s) found:")
-    message(list_out(discard_p, final = "and", per_line = 5, indent = "  ", wrap = '"'))
+    message(list_out(discard_params, final = "and", per_line = 5, indent = "  ", wrap = '"'))
     message("Do you wish to proceed with removing these nodes (Y/N)?")
     answer = readline(prompt = "")
     if (!(tolower(answer) %in% c(acceptable_yes, acceptable_no))) {
@@ -46,10 +46,10 @@ post_remove = function(post, p, ask = TRUE, auto_escape = TRUE) {
 
   if (tolower(answer) %in% acceptable_yes) {
     # extract the names of all nodes
-    all_p = get_params(post, type = "all")
+    all_params = get_params(post, type = "all")
 
     # get the names of the nodes to keep
-    keep_p = all_p[!(all_p %in% discard_p)]
+    keep_params = all_params[!(all_params %in% discard_params)]
 
     # extract the iteration ids: chain and iteration numbers
     ids = id_mat(post)
@@ -58,11 +58,10 @@ post_remove = function(post, p, ask = TRUE, auto_escape = TRUE) {
 
     # extract the samples from the right nodes in each chain, place these into list elements
     post_sub_list = lapply(post, function(x) {
-      mat = as.matrix(x[,keep_p], nrow = n_iter, ncol = length(keep_p))
-      colnames(mat) = keep_p
+      mat = as.matrix(x[,keep_params], nrow = n_iter, ncol = length(keep_params))
+      colnames(mat) = keep_params
       mat
-    }
-    )
+    })
 
     # store the samples as a matrix
     post_sub_matx = NULL
@@ -74,7 +73,7 @@ post_remove = function(post, p, ask = TRUE, auto_escape = TRUE) {
 
     # print a message
     message("Node(s) discarded:")
-    message(list_out(discard_p, final = "and", per_line = 5, indent = "  ", wrap = '"'))
+    message(list_out(discard_params, final = "and", per_line = 5, indent = "  ", wrap = '"'))
 
     # return the output
     return(post_out)
