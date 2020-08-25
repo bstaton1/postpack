65aa7dac4a533744a470e150790809e643cafdbd
Ben Staton <statonbe@gmail.com>
2020-08-24 19:39:36 -0700

    Finalize all functions in utils.R and docs

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,11 @@
-#' Insert escapes on regex brackets
-#'
-#' @param params a character vector
-#' @details searches the contents of a string for the occurrence of a
-#'  square bracket or two, and inserts the necessary escapes for searching
-#'  via regular expression
-#' @note this is NOT a function users will generally use.
+#' @title Insert escapes on regex brackets
+#' @param params A character vector with length >= 1.
+#' @details Searches the contents of a string for the occurrence of a
+#'  square bracket or two, and inserts the necessary escapes for pattern matching
+#'  via regular expressions.
+#' @note This is NOT a function users will generally use directly.
+#' @return A character vector with all brackets escaped. For example,
+#'   `"a[1]"` becomes `"a\\[1\\]"`
 
 ins_regex_bracket = function(params) {
   out = stringr::str_replace(params, "\\[", "\\\\[")
@@ -12,13 +13,14 @@ ins_regex_bracket = function(params) {
   return(out)
 }
 
-#' Remove escapes on regex brackets
-#'
-#' @param params a character vector
-#' @details searches the contents of a string for the occurrence of a
+#' @title Remove escapes on regex brackets
+#' @param params A character vector with length >= 1.
+#' @details Searches the contents of a string for the occurrence of a
 #'  square bracket or two (that has been escaped), and removes the escaping
-#'  that was necessary for regular expression matching
-#' @note this is NOT a function users will generally use.
+#'  that was necessary for matching via regular expressions.
+#' @note This is **not** a function users will generally use directly.
+#' @return A character vector with all brackets escaped. For example,
+#'   `"a\\[1\\]"` becomes `"a[1]"`.
 
 rm_regex_bracket = function(params) {
   out = stringr::str_replace(params, "\\\\\\[", "\\[")
@@ -26,14 +28,14 @@ rm_regex_bracket = function(params) {
   return(out)
 }
 
-#' Insert the symbols to lock in a string for matching
-#'
-#' To ensure that a regular expression will match exactly,
-#' it's necessary to specify so - which is what this function does.
-#'
-#' @param params a character vector on to which a carrot and dollar sign
-#'   will be pasted to if not already present
-#' @note this is NOT a function users will generally use.
+#' @title Insert the symbols to lock in a string for matching
+#' @description To ensure that a regular expression will match exactly,
+#'   it's necessary to specify so.
+#' @param params A character vector with length >= 1 to paste a `^` and `$`
+#'   (if not already present) to lock in the match.
+#' @note This is **not** a function users will generally use directly.
+#' @return A character vector with locking anchors inserted to force an exact match. For example,
+#'   `"a\\[1\\]"` becomes `"^a\\[1\\]$"`.
 
 ins_regex_lock = function(params) {
   if (!stringr::str_detect(params, "\\^")) out = paste("^", params, sep = "")
@@ -41,15 +43,13 @@ ins_regex_lock = function(params) {
   return(out)
 }
 
-#' Remove the symbols that lock in a string for matching
-#'
-#' To ensure that a regular expression will match exactly,
-#' it's necessary to specify so - this function undoes the work of
-#' \code{\link{ins_regex_lock}} - currently just for prettier printing of node names
-#'
-#' @param params a character vector on to which a carrot and dollar sign
-#'   will be removed from if present
-#' @note this is NOT a function users will generally use.
+#' @title Remove the symbols that lock in a string for matching
+#' @description Undoes the work of [ins_regex_lock()].
+#' @param params A character vector with length >= 1 to remove a `^` and `$` from
+#'   (if present).
+#' @note This is **not** a function users will generally use directly.
+#' @return A character vector with locking anchors inserted to force an exact match. For example,
+#'   `"^a\\[1\\]$"` becomes `"a\\[1\\]"`.
 
 rm_regex_lock = function(params) {
   out = stringr::str_replace(params, "\\^", "")
@@ -61,8 +61,9 @@ rm_regex_lock = function(params) {
 #' @description Removes square brackets, numbers, and commas that represent
 #'   the index of the node element in question. Returns just the node name.
 #' @param params A character vector with length >= 1 containing node names.
-#' @note This is NOT a function users will generally use.
+#' @note This is **not** a function users will generally use directly.
 #' @return A character vector with the same length as `params`, with no indices included.
+#'   For example, `"a[1]"` becomes `"a"`.
 #' @examples
 #' postpack:::drop_index("a[1]")
 #' postpack:::drop_index(c("a[1]", "a[2]"))
@@ -72,15 +73,13 @@ drop_index = function(params) {
   stringr::str_replace(params, "\\[.+\\]", "")
 }
 
-#' Adds a title between two figures
-#'
-#' Used by \code{\link{diag_plots}} to place a common
-#' title over top of two figures: one trace and one density
-#' for a given node.
-#'
-#' @param text character vector length == 1. The text string to
-#'   include as a title
-#' @note this is NOT a function users will generally use.
+#' @title Add a title between two figures
+#' @description Used by [diag_plots()] to place a common
+#'   title over top of two figures: one density and one trace
+#'   for a given node.
+#' @param text A character vector length == 1. The text string to
+#'   include as a title.
+#' @note This is **not** a function users will generally use directly.
 
 mytitle = function(text) {
   usr = par("usr"); xdiff = diff(usr[1:2]); ydiff = diff(usr[3:4])
@@ -90,8 +89,8 @@ mytitle = function(text) {
 
 #' @title Extract chain and iteration IDs for each sample
 #' @param post A [`mcmc.list`][coda::mcmc.list] object.
-#' @return A matrix with columns`"CHAIN"` and `"ITER"`.
-#' @note This is NOT a function users will generally use.
+#' @note This is **not** a function users will generally use directly.
+#' @return A matrix with columns `"CHAIN"` and `"ITER"`.
 
 id_mat = function(post) {
   # stop if post isn't mcmc.list
